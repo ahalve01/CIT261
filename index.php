16245ae3fa833cd065085d79e5622605c3cdb59c
ahalve01 <adamhalverson@gmail.com>
2016-05-24 21:22:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en-us">
+    <head>
+        <title>Home | Adamstreasury.com</title>
+        <meta charset="UTF-8">        
+        <meta name="author" content="Adam Halverson">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum scale=1.0" >        
+    </head>
+    <body id="home">
+       
+            <article>
+                <h1>CIT 261 Portfolio</h1>
+                <h2><a href="CSSAnnimationCopy.html">CSS Animation Copy</a></h2>
+                <h2><a href="CSSAnnimationSelf.html">CSS Animation Self</a></h2>
+                <h2><a href="DOMexample.html">DOM Example</a></h2>
+                <h2><a href="DOMSelf.html">My DOM</a></h2>
+                <h2><a href="JavaScriptObjectsSample.html">JavaScript Object Example</a></h2>
+                <h2><a href="JavaSCriptObjectSelf.html">JavaScript Object Self</a></h2>
+                <h2><a href="CSS3Transitions.html">CSS3 Transition Demo</a></h2>
+                <h2><a href="CSS3TransitionSelf.html">CSS3 Transition Self</a></h2>
+                <h2><a href="numberofhitscopy.html">Number of Hits Example </a></h2>
+                <h2><a href="LocalStorageDemoSelf.html">Local Storage Demo Self</a></h2>
+                
+            </article> 
+
+        <footer>
+            <p>Adam's Treasury &amp 2016</p>            
+        </footer>
+</body>
+</html>
